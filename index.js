924b4466c5714cfef12d85a61569ecd6290028bb
DanielleWalker35 <yeller22@gmail.com>
2018-05-17 13:21:00 -0600

    Change to port number on server

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-const PORT = 8080 || process.env.PORT;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.static(path.join(__dirname, "client", "build")));
 app.get("*", (req, res) => {
